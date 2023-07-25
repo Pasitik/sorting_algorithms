9f8d161ed448267d25d56e3c60ab962b56f0fc1c
DD1C74707 <gracevalley101@gmail.com>
2023-07-25 00:07:34 -0700

    update selelctions sort algorithm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+#include <stdio.h>
 
 /**
  * selection_sort - Sorts an array of integers in ascending order
@@ -10,26 +11,28 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i = 0, j = 0, temp =0, min_value = 0;
+	size_t i = 0, j, temp, min_value = 0;
 
-    while (i < size)
-    {
-        min_value = i;
-        temp = 0;
-        j = i + 1;
-        while (j < size)
-        {
-            if (array[j] < array[min_value])
-            {
-                min_value = j;
-            }
-            j++;
-        }
-        temp = array[i];
-        array[i] = array[min_value];
-        array[min_value] = temp;
-        print_array(array, size);
-        i++;
-    }
+	while (i < size - 1)
+	{
+		min_value = i;
+		j = i + 1;
+		while (j < size)
+		{
+			if (array[j] < array[min_value])
+			{
+			min_value = j;
+			}
+			j++;
+		}
+		if (min_value != i)
+		{
+			temp = array[i];
+			array[i] = array[min_value];
+			array[min_value] = temp;
+			print_array(array, size);
+		}
+		i++;
+	}
 }
 
