d884ffa55a9193f8e0ca6a93408e44f03118e9c9
Paul Asitik <paasitik@gmail.com>
2023-07-24 23:11:21 -0700

    update

---
+++ 3-quick_sort.c
@@ -0,0 +1,90 @@
+#include "sort.h"
+
+/**
+ * swap - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * partition - Order a subset of an array of integers according to
+ *                    the lomuto partition scheme (last element as pivot).
+ * @array: The array of integers.
+ * @size: The size of the array.
+ * @l: The starting index of the subset to order.
+ * @r: The ending index of the subset to order.
+ *
+ * Return: The final partition index.
+ */
+int partition(int *array, size_t size, int l, int r)
+{
+	int *pivot, above, below;
+
+	pivot = array + r;
+	for (above = below = l; below < r; below++)
+	{
+		if (array[below] < *pivot)
+		{
+			if (above < below)
+			{
+				swap(array + below, array + above);
+				print_array(array, size);
+			}
+			above++;
+		}
+	}
+
+	if (array[above] > *pivot)
+	{
+		swap(array + above, pivot);
+		print_array(array, size);
+	}
+
+	return (above);
+}
+
+/**
+ * lomuto_sort - Implement the quicksort algorithm through recursion.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ * @left: The starting index of the array partition to order.
+ * @right: The ending index of the array partition to order.
+ *
+ * Description: Uses the Lomuto partition scheme.
+ */
+void QuickSort(int *array, size_t size, int l, int r)
+{
+	int part;
+
+	if (r - l > 0)
+	{
+		part = partition(array, size, l, r);
+		QuickSort(array, size, l, part - 1);
+		QuickSort(array, size, part + 1, r);
+	}
+}
+
+/**
+ * quick_sort - Sort an array of integers in ascending
+ *              order using the quicksort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Uses the Lomuto partition scheme. Prints
+ *              the array after each swap of two elements.
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	QuickSort(array, size, 0, size - 1);
+}
